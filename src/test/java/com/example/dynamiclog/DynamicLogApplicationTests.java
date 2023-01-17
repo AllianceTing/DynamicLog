be5a98a2b5fa92fa420ab6d2aa70af2c9db85e28
张震霆 <2426446427@qq.com>
2023-01-17 19:38:23 +0800

    初始化空项目

---
+++ src/test/java/com/example/dynamiclog/DynamicLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynamiclog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicLogApplicationTests {
+
+@Test
+void contextLoads() {
+}
+
+}
